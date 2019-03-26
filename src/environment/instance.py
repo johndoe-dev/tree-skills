5fb27d0eddb4cfbc68d9169a3f7021810a9a684f
Jonathan Ullindah <jonathan.ullindah@open-groupe.com>
2019-03-27 00:18:17 +0100

    Add Databases connection to environmenet

--- src/environment/instance.py
+++ src/environment/instance.py
@@ -6,25 +6,6 @@ basedir = os.path.normpath(os.path.join(os.path.dirname(__file__), "../../.env")
 load_dotenv(basedir)
 
 
-# # Load the development "mode". Use "development" if not specified
-# env = os.environ.get("PYTHON_ENV", "development")
-#
-# # Configuration for each environment
-# # Alternatively use "python-dotenv"
-# all_environments = {
-#     "development": { "port": 5000, "host": "0.0.0.0", "debug": True, "swagger-url": "/" },
-#     "production": { "port": 8080, "host": "0.0.0.0", "debug": False, "swagger-url": None  }
-# }
-#
-# # The config for the current environment
-# # environment_config = all_environments[env]
-# config = {
-#     "environment_config": all_environments[env],
-#     "database": {
-#         "url": os.environ.get("URL")
-#     }
-# }
-
 class Config(object):
     # Load the development "mode". Use "development" if not specified
     __env = os.environ.get("PYTHON_ENV", "development")
