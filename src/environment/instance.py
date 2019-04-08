214b4ef3ed7eb1e8e0ca8b622a743019f3325337
Jonathan Ullindah <jonathan.ullindah@open-groupe.com>
2019-04-08 19:50:02 +0200

    Add DevelopmentConfig and TestingConfig

--- src/environment/instance.py
+++ src/environment/instance.py
@@ -7,22 +7,32 @@ load_dotenv(basedir)
 
 
 class Config(object):
-    # Load the development "mode". Use "development" if not specified
-    __env = os.environ.get("PYTHON_ENV", "development")
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "secret_key"
+    DEBUG = False
+    PORT = 5000
+    HOST = "0.0.0.0"
+    # Database auth
+    NEO4J_DATABASE_USERNAME = "neo4j"
+    NEO4J_DATABASE_PWD = "test"
+    SWAGGER_URL = "/"
 
-    __all_environments = {
-        "development": {"port": 5000, "host": "0.0.0.0", "debug": True, "swagger-url": "/"},
-        "production": {"port": 8080, "host": "0.0.0.0", "debug": False, "swagger-url": None}
-    }
 
-    # Environments of api
-    PORT = __all_environments[__env]["port"]
-    HOST = __all_environments[__env]["host"]
-    DEBUG = __all_environments[__env]["debug"]
-    SWAGGER_URL = __all_environments[__env]["swagger-url"]
+class DevelopmentConfig(Config):
+    DEBUG = True
+    # Database connections
+    NEO4J_DATABASE_URL = os.environ.get("NEO4J_DATABASE_URL") + "/db/data/"
+    NEO4J_DATABASE_IP = os.environ.get("NEO4J_DATABASE_IP") + " /db/data/"
 
-    # Databases connection
-    NEO4J_URL = os.environ.get("NEO4J_URL") + "/db/data/"
-    NEO4J_IP = os.environ.get("NEO4J_IP") + " /db/data/"
-    NEO4J_USERNAME = "neo4j"
-    NEO4J_PWD = "test"
+
+class TestingConfig(Config):
+    DEBUG = True
+    TESTING = True
+    # Database connections
+    NEO4J_DATABASE_URL = os.environ.get("NEO4J_DATABASE_URL_TEST") + "/db/data/"
+    NEO4J_DATABASE_IP = os.environ.get("NEO4J_DATABASE_IP_TEST") + " /db/data/"
+
+
+config = dict(
+    dev=DevelopmentConfig,
+    test=TestingConfig
+)
