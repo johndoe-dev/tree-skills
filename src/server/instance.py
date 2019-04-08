c12e44024bc8657297593d6359a6295251455899
Jonathan Ullindah <jonathan.ullindah@open-groupe.com>
2019-04-08 19:57:59 +0200

    Update server

--- src/server/instance.py
+++ src/server/instance.py
@@ -1,25 +1,56 @@
 from flask import Flask, Blueprint
-from flask_restplus import Api, Resource, fields
-from environment.instance import Config
+from flask_script import Manager
+from flask_restplus import Api
+from environment.instance import config
+from database.neo4j import Database
+
 
 class Server(object):
-    def __init__(self):
-        self.app = Flask(__name__)
-        self.app.config.from_object(Config)
+    def __init__(self, config):
+        self._app = Flask(__name__)
+        self._app.config.from_object(config)
+        self._database = Database(self._app)
         self.blueprint = Blueprint('api', __name__, url_prefix='/api/1')
-        self.api = Api(self.blueprint,
+        self._api = Api(
+            self.blueprint,
             version='1.0',
             title='Sample Book API',
             description='A simple Book API',
-            doc = self.app.config["SWAGGER_URL"]
+            doc=self._app.config["SWAGGER_URL"],
+            ordered=True
         )
+
+        self._app.register_blueprint(self.blueprint)
+
+        self.manager = Manager(self._app)
+
+    @property
+    def app(self):
+        return self._app
+
+    @property
+    def api(self):
+        return self._api
+
+    @property
+    def database(self):
+        return self._database
+
+    @property
+    def graph(self):
+        return self._database.graph
+
+    def add_namespaces(self, *namespaces):
+        for namespace in namespaces:
+            self._api.add_namespace(namespace)
 
     def run(self):
         # self.app.run()
-        self.app.run(
-                debug = self.app.config["DEBUG"],
-                port = self.app.config["PORT"],
-                host = self.app.config["HOST"]
+        self._app.run(
+                debug=self.app.config["DEBUG"],
+                port=self.app.config["PORT"],
+                host=self.app.config["HOST"]
             )
 
-server = Server()
+
+server = Server(config["dev"])
