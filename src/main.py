085f67322f63f5d9e0b157203f951c5319155475
Jonathan Ullindah <jonathan.ullindah@open-groupe.com>
2019-04-08 19:59:17 +0200

    Add command run, create_all, delete_all and test

--- src/main.py
+++ src/main.py
@@ -1,16 +1,33 @@
-# Need to import all resources
-# so that they register with the server
-from resources.book import ns as book_ns
-from resources.person import ns as person_ns
+import unittest
+from resources import person_ns, techno_ns, team_ns
 from server.instance import server
 
 
-def main():
-    server.api.add_namespace(book_ns)
-    server.api.add_namespace(person_ns)
-    server.app.register_blueprint(server.blueprint)
+@server.manager.command
+def run():
+    server.add_namespaces(person_ns, techno_ns, team_ns)
     server.run()
 
 
+@server.manager.command
+def create_all():
+    server.database.create_all()
+
+
+@server.manager.command
+def delete_all():
+    server.database.delete_all()
+
+
+@server.manager.command
+def test():
+    """Runs the unit tests."""
+    tests = unittest.TestLoader().discover('src/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+
 if __name__ == '__main__':
-    main()
+    server.manager.run()
