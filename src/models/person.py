ad3ac5ff249e91d806249f2a9de59008d9b11a94
Jonathan Ullindah <jonathan.ullindah@open-groupe.com>
2019-04-08 19:54:16 +0200

    Update PersonModel

--- src/models/person.py
+++ src/models/person.py
@@ -1,10 +1,67 @@
-from flask_restplus import fields
-from server.instance import server
+from .graph_object import Person
+from .model import GraphModel
 
 
-person = server.api.model('Person', {
-    'name': fields.String(required=True, min_length=1, max_length=200, description='Person name'),
+class PersonModel(GraphModel):
+    """Person model to handle person node and related object"""
+    def __init__(self, graph, api):
+        super().__init__(graph, api, Person)
 
-})
+    def get(self, name, one=True):
+        """get one or more person(s)"""
+        person = list(self.graph_object.select(self.graph).where("_.name =~ '{}.*'".format(name)))
+        if one:
+            person = self.graph_object.select(self.graph, name).first()
+
+        return person
+
+    def post(self, data):
+        """Add one person"""
+        person = self.graph_object()
+        person.name = data.get("name", "")
+        self.graph.push(person)
+        return data
+
+    def put(self, name, data):
+        """Update one person"""
+        selection = self.get(name)
+        if not selection:
+            return None
+        if data:
+            selection.name = data.get("name", "")
+            self.graph.push(selection)
+            return selection
+
+    def delete(self, name):
+        """Delete one person"""
+        selection = self.get(name)
+        if not selection:
+            return None
+        self.graph.delete(selection)
+        return selection
+
+    def relation(self, rel, name=None, target=None):
+        """Get related GraphObject"""
+        selection = self.lists
+        if name:
+            selection = self.get(name, one=False)
+        return super().relation(rel=rel, selection=selection, target=target)
+
+    def add_relation(self, rel, source_name, target):
+        """Create relation with GraphObject"""
+        selection = self.get(source_name)
+        if super().add_relation(rel, selection, target):
+            return self.relation(rel, source_name)
+        return None
+
+    def delete_relation(self, rel, source_name, target):
+        """Delete relation with GraphObject"""
+        selection = self.get(source_name)
+        if not target:
+            selection.__getattribute__(rel).remove_all()
+        else:
+            selection.__getattribute__(rel).remove(target)
+
+        self.graph.push(selection)
 
 
