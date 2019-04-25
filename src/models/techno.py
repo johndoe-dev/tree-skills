d206df8b05188b4189a4e5c0e01723073e35cc1d
Jonathan Ullindah <jonathan.ullindah@open-groupe.com>
2019-04-25 13:04:29 +0200

    Update __init__ replace graph and api by server

--- src/models/techno.py
+++ src/models/techno.py
@@ -4,8 +4,8 @@ from .model import GraphModel
 
 class TechnoModel(GraphModel):
     """Techno model to handle techno node and related object"""
-    def __init__(self, graph, api):
-        super().__init__(graph, api, Techno)
+    def __init__(self, server):
+        super().__init__(server, Techno)
 
     def get(self, name, one=True):
         """Get one or more techno"""
